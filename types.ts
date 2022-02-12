155454dc228cead4174b72d99bbca4b3d3a86281
EddyBautista <bautista.j.eddy@gmail.com>
2022-02-11 22:13:13 -0600

    set up files for neovim

---
+++ types.ts
@@ -0,0 +1,7 @@
+// The main benfit of typescript is that it enables you to add static types to js,
+// types place contraints to program entities like functions, vars, and props so compilers
+// and dev tools can offer better verification.
+
+let x: number; // Explicitly declares c as a number type.
+let y = 1; // Implicitly delcares y as a number type.
+let z; //* Declares z without initializing it
